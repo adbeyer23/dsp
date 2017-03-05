4d0465e075068978dfbc4567a7cc5e245c9f5aa1
adbeyer23 <aarbeyer@gmail.com>
2017-03-05 17:16:51 -0600

    Finishing datetime

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,26 @@
 # Hint:  use Google to find python function
 
 ####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+date_startA = '01-02-2013'  
+date_stopA = '07-28-2015'   
 
 ####b)  
-date_start = '12312013'  
-date_stop = '05282015'  
+date_startB = '12312013'  
+date_stopB = '05282015'  
 
 ####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+date_startC = '15-Jan-1994'  
+date_stopC = '14-Jul-2015'  
+
+formatA = "%m-%d-%Y"
+formatB = "%m%d%Y"
+formatC = "%d-%b-%Y"
+
+def find_date_diff(date_start, date_stop, format1):
+    timedelta_start = datetime.datetime.strptime(date_start, format1)
+    timedelta_end = datetime.datetime.strptime(date_stop, format1)
+    return timedelta_end - timedelta_start
+  
+print(find_date_diff(date_startA, date_stopA, formatA))
+print(find_date_diff(date_startB, date_stopB, formatB))
+print(find_date_diff(date_startC, date_stopC, formatC))
