d3f1bd377195fe4e60400eff3c8b2b622d05c9a3
adbeyer23 <aarbeyer@gmail.com>
2017-03-11 12:05:35 -0600

    Finishing list problems

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -15,8 +15,11 @@ def match_ends(words):
     >>> match_ends(['aaa', 'be', 'abc', 'hello'])
     1
     """
-    raise NotImplementedError
-
+    count = 0
+    for x in words:
+        if len(x) >= 2 and x[0]== x[-1]:
+            count +=1
+    return count
 
 def front_x(words):
     """
@@ -32,7 +35,11 @@ def front_x(words):
     >>> front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark'])
     ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
     """
-    raise NotImplementedError
+    x_list = [x for x in words if x[0] == "x"]
+    for a in x_list:
+        if a in words:
+            words.remove(a)
+    return sorted(x_list) + sorted(words)
 
 
 def sort_last(tuples):
@@ -49,8 +56,7 @@ def sort_last(tuples):
     >>> sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)])
     [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
     """
-    raise NotImplementedError
-
+    return sorted(tuples, key= lambda x: x[-1])
 
 def remove_adjacent(nums):
     """
@@ -68,7 +74,13 @@ def remove_adjacent(nums):
     >>> remove_adjacent([])
     []
     """
-    raise NotImplementedError
+    numbers = []
+    if len(nums) != 0:
+        numbers.append(nums[0])
+        for x in nums[1:]:
+            if x != numbers[-1]:
+                numbers.append(x)
+    return numbers
 
 
 def linear_merge(list1, list2):
@@ -85,4 +97,16 @@ def linear_merge(list1, list2):
     >>> linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb'])
     ['aa', 'aa', 'aa', 'bb', 'bb']
     """
-    raise NotImplementedError
+    merged = []
+    while len(list1) and len(list2):
+        if list1[0] < list2[0]:
+            merged.append(list1[0])
+            del list1[0]
+        else:
+            merged.append(list2[0])
+            del list2[0]
+    if len(list1):
+        merged = merged + list1
+    elif len(list2):
+        merged = merged + list2
+    return merged
