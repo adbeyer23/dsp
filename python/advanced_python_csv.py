07e4c95d016bcc271d7e096c2418e206bd41a909
aaron beyer <aaronbeyer@aarons-MBP.T-mobile.com>
2017-03-12 17:54:55 -0500

    fixing csv stuff

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,8 +1,8 @@
 from advanced_python_regex import emails
-import cvs
-file = open("emails.csv", "rb")
+import csv
+file = open("emails.csv", "w")
 reader = csv.reader(file)
 writer = csv.writer(file)
 for email in emails:
-  writer.writerow(email)
+  writer.writerow([email])
 file.close()
