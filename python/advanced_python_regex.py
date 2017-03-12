7c9e05fe7bc8eb9bb0df2c880b31adf1abf7252b
adbeyer23 <aarbeyer@gmail.com>
2017-03-12 17:39:14 -0500

    Complete??will test

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,22 @@
-PLACE YOUR CODE HERE
+import pandas as pd
+faculty = pd.read_csv("faculty.csv")
+faculty = faculty.rename(columns = lambda x: x.strip())
+faculty["degree"] = faculty["degree"].apply(lambda x: x.replace(".", ""))
+faculty["degree"] = faculty["degree"].apply(lambda x: x.strip())
+faculty["degree"] = faculty["degree"].apply(lambda x: x.split())
+degrees = list(faculty.degree)
+from collections import Counter
+degrees_fixed = []
+for x in degrees:
+    for y in x:
+        degrees_fixed.append(y)
+Counter(degrees_fixed)
+faculty.set_value(24,  "title", "Assistant Professor of Biostatistics")
+
+emails = list(faculty.email)
+
+email_domains = [x.split("@")[-1] for x in emails]
+from collections import Counter
+Counter(email_domains)
+
+
