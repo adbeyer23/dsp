31ce6fd231b5984558e27d97a29f589464632a43
adbeyer23 <aarbeyer@gmail.com>
2017-03-12 17:33:06 -0500

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,8 @@
 from advanced_python_regex import emails
+import cvs
+file = open("emails.csv", "rb")
+reader = csv.reader(file)
+writer = csv.writer(file)
+for email in emails:
+  writer.writerow(email)
+file.close()
