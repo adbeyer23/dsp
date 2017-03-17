36db811a81e0c0232ec3b624bc66bdc95c81b175
adbeyer23 <aarbeyer@gmail.com>
2017-03-16 21:09:21 -0500

    made code a bit clearer

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import pandas as pd
 faculty = pd.read_csv("faculty.csv")
 faculty = faculty.rename(columns = lambda x: x.strip())
@@ -16,7 +17,7 @@ faculty.set_value(24,  "title", "Assistant Professor of Biostatistics")
 emails = list(faculty.email)
 
 email_domains = [x.split("@")[-1] for x in emails]
-from collections import Counter
+
 Counter(email_domains)
 
 
